052e8f08a8087b00369e9a580adc14ed817b146c
Manasha30 <83446024+Manasha30@users.noreply.github.com>
2022-03-02 12:19:39 +0530

    added new file

--- pharmacy-react/src/App.js
+++ pharmacy-react/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './Homepage';
 
 function App() {
   return (
     <div className="App">
      <h1> Pharmacy Inventory</h1>
+     <Homepage/>
     </div>
   );
 }
