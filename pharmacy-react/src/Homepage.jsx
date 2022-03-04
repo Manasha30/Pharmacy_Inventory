cf087f4712576b89acc72a7c491f24c1bf66bcb8
Manasha30 <83446024+Manasha30@users.noreply.github.com>
2022-03-04 07:19:44 +0530

    login update

--- pharmacy-react/src/Homepage.jsx
+++ pharmacy-react/src/Homepage.jsx
@@ -4,29 +4,18 @@ class Homepage extends Component {
     state = {  } 
     render() {
                 return (
-        
-                    <div>
-                      <h1 class='solid'>Pharmacy Inventory</h1>
-
-                       <p id="demo">The pharmacy inventory process helps track the amount of medication<br></br> that is available and helps reduce costs and monitor expired medications</p>
-                       
-                        <center><a href="/Login"><button type="button"><span></span>LOGIN</button> </a></center>
-                        <center><a href="/Register"><button type="button"><span></span>REGISTER</button> </a></center>                     
                 
-                     <div className ="homepage"> 
+                    <div className ="homepage">
             
                       <h1 className='solid'>Pharmacy Inventory</h1>
-                      
                       <div class="button-container">
                         <a href="/Login"><button type="button"><span></span>LOGIN</button> </a>
                         <a href="/Register"><button type="button"><span></span>REGISTER</button> </a>  
-                              </div>
-                              </div>   
+                        </div>              
                     </div>   
             
                         );
             }
+        }
         
-}
-export default Homepage;
-        
+        export default Homepage;
