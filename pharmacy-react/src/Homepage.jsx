428b1f5a4189a426ea9f3e6f2c6e5992e77b8e5f
Akshaya-dot <85672969+Akshaya-dot@users.noreply.github.com>
2022-04-17 18:33:22 +0530

    updated

--- pharmacy-react/src/Homepage.jsx
+++ pharmacy-react/src/Homepage.jsx
@@ -10,7 +10,10 @@ class Homepage extends Component {
                       <h1 className='solid'>Pharmacy Inventory</h1>
                       <div class="button-container">
                         <a href="/Login"><button type="button"><span></span>LOGIN</button> </a>
-                        <a href="/Register"><button type="button"><span></span>REGISTER</button> </a>  
+                        <a href="/Register"><button type="button"><span></span>REGISTER</button> </a> 
+                        <a href="/products"><button type="button"><span></span>PRODUCTS</button> </a>
+                        <a href="/StockEvents"><button type="button"><span></span>STOCKEVENTS</button> </a>
+                        <a href="/MedicineInventory"><button type="button"><span></span>MEDICINE INVENTORY</button> </a>
                         </div>              
                     </div>   
             
